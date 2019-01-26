c51ff472173986c2bd2da8822ae12b4cecf81a3a
DESKTOP-OG3SE7I\Bor <b.s.greve@students.uu.nl>
2019-01-26 23:02:57 +0100

    Base page

---
+++ Home.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<img src="img\test\WEEB.png" alt="WEEB">
+<br>
+Logically Minded // Creative At Heart // Kill Yourself
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+<meta http-equiv="refresh" content="0; URL='https://www.borgreve.com/Home.php'" />
+
+</body>
+</html>
