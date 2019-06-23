53ad6c923091daeff83de49c98cfcf902a4207b6
DESKTOP-OG3SE7I\Bor <b.s.greve@students.uu.nl>
2019-06-23 23:44:44 +0200

    Updated content and added real descriptions

--- Home.php
+++ Home.php
@@ -1,9 +1,180 @@
 <!DOCTYPE html>
 <html>
-<body>
-<img src="img\test\WEEB.png" alt="WEEB">
-<br>
-Logically Minded // Creative At Heart // Kill Yourself
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+
+
+
+<div class="container">
+
+    <div class="contentheader border-bottom mb-4">
+        Portfolio site
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="columntitle mb-4">
+                Programming
+            </div>
+
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">AR Business Card</h5>
+                        <div align="center" class="embed-responsive embed-responsive-16by9">
+                            <video autoplay loop muted class="embed-responsive-item">
+                            <source src="/vid/AR Business Card.mp4" type="video/mp4">
+                            </video>
+                        </div>
+                    <p class="card-text">I made a simple business card AR app for the sales team of my current company, which they could show to potential clients to make them consider ways in which AR could be useful to them.</p>
+                </div>
+            </div>
+
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Parallel world defence</h5>
+                        <div align="center" class="embed-responsive embed-responsive-16by9">
+                            <video autoplay loop muted class="embed-responsive-item">
+                            <source src="/vid/splitty explodey cropped.mp4" type="video/mp4">
+                            </video>
+                        </div>
+                    <p class="card-text">A game made in 48 hours for Ludum Dare 41. The player must keep enemies from reaching the bottom of the screen, but every ability the player has splits the world into four distinct possibilities. The goal is to save as many worlds as possible.</p>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Corridor Bijsturing</h5>
+                        <div align="center" class="embed-responsive embed-responsive-16by9">
+                            <video autoplay loop muted class="embed-responsive-item">
+                            <source src="/vid/corridor.mp4" type="video/mp4">
+                            </video>
+                        </div>
+                    <p class="card-text">In a small team we made a training simulation for the Dutch railway innovation division. It uses real data provided by ProRail to let the players experience the tribulations of a train traffic controller. The game is played by multiple people at the same time using different touch monitors, so even the communication elements of the job are retained.</p>
+                </div>
+            </div>
+            
+        </div>
+        <div class="col-sm-4">
+            <div class="columntitle mb-4">
+                Hobby
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Models</h5>
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            
+                            <div class="carousel-item active">
+                            <img class="d-block w-100" src="/img/models/DSC_3601.jpg" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/models/DSC_3603.jpg" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/models/DSC_3136.JPG" alt="Third slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/models/DSC_4275.jpg" alt="Fourth slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/models/sw.jpg" alt="Fifth slide">
+                            </div>
+                        </div>
+                    </div>
+                    <p class="card-text">I like making things with my hands as well as with code. I mostly build and paint sci-fi models, both from existing properties and of my own design.</p>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Photography</h5>
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            
+                            <div class="carousel-item active">
+                            <img class="d-block w-100" src="/img/photos/DSC_6725.jpg" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_5635.jpg" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_6068.jpg" alt="Third slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_7292.jpg" alt="Fourth slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_7441.jpg" alt="Fifth slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_7510.jpg" alt="Sixth slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_3953.jpg" alt="Seventh slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_4422.jpg" alt="Eigth slide">
+                            </div>
+                        </div>
+                    </div>
+                    <p class="card-text">Be it here in Amsterdam or on the other side of the world in Japan, taking pictures that convey my aesthetic sensibilities is another outlet for creativity.</p>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">日本語</h5>
+                    
+                    <p class="card-text">日本語を独学してるから試験は受け取ったことないけど、日本にいる時日本語しか話さない日が多いので日常会話くらいは問題なくできる。弱点はカジュアルの話方と手書きは全く練習してないとこ。
+                        いつもネット友達と話してるから今の状態は仕方ないと思うけど、日本の会社に仕事したら結構早めにちゃんとした日本語にもなれるはず。
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="columntitle mb-4">
+                Life
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Education</h5>
+                    <p class="card-text">I graduated in the summer of 2018 with a bachelor's in computer science from the University of Utrecht. I followed a specific track focusing on game technology
+                    which required a set of specific electives focusing on 3d rendering to be completed. In addition to game technology and general computer science, I took electives in information sciences.</p>
+                    <p class="card-text cardalineatitle">Notable classes</p>
+                    <p class="card-text classlist">Imperative Programming, Algorithms & Complexity, Data structures, Databases, Concurrency, Functional programming, Artificial Intelligence, Machine Learning, Neural Networks, Languages & Compilers, Computer Graphics, 3D Modelling, Game Design, Software Testing & Verification, Design Patterns & UML, Cognition & Emotion, Image Processing </p>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Personality</h5>
+                    <p class="card-text">Calm and collected, I am good at de-escalating conflicts between other parties. I tend to resolve conflicts I am part of with kindness and patience,
+                        and don't get angry at all. I have been praised by co-workers and friends alike for my "cynical positivism", and can turn any bad situation into an observation about the
+                        absurdity of human nature. 
+                    </p>
+                    <p class="card-text">
+                        My strong suit in human interaction is empathy. I place great importance in properly understanding not just what a person technically said, but what they meant
+                        and why they said whatever they did. 
+                    </p>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Goals</h5>
+                    <p class="card-text">My passion and goal for the future lies in using technology to interface with the real world to create new artistic experiences. Whether it be through
+                        experimental video games, AR applications or room-sized art installations, I want to create new forms of expression and let people experience things they normally wouldn't.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ meme.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+
+
+
+<div class="container">
+
+    <div class="contentheader border-bottom mb-4">
+        Portfolio site
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4">
+            <div class="columntitle mb-4">
+                Programming
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Something cool I made</h5>
+                        <div align="center" class="embed-responsive embed-responsive-16by9">
+                            <video autoplay loop muted class="embed-responsive-item">
+                            <source src="/vid/splitty explodey cropped.mp4" type="video/mp4">
+                            </video>
+                        </div>
+                    <p class="card-text">Here is an explanation of a cool thing I made.</p>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Some place cool I worked</h5>
+                    <p class="card-text">This very cool company hired me so you should too.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="columntitle mb-4">
+                Hobby
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Creative project that I finished</h5>
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            
+                            <div class="carousel-item active">
+                            <img class="d-block w-100" src="/img/models/DSC_3601.jpg" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/models/DSC_3603.jpg" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/models/DSC_3136.JPG" alt="Third slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/models/DSC_4275.jpg" alt="Fourth slide">
+                            </div>
+                        </div>
+                    </div>
+                    <p class="card-text">Look I'm a creative boy.</p>
+                </div>
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Artsy picture I took</h5>
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            
+                            <div class="carousel-item active">
+                            <img class="d-block w-100" src="/img/photos/DSC_6725.jpg" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_5635.jpg" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_6068.jpg" alt="Third slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_7292.jpg" alt="Fourth slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_7441.jpg" alt="Fourth slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_7510.jpg" alt="Fourth slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_3953.jpg" alt="Fourth slide">
+                            </div>
+                            <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/photos/DSC_4422.jpg" alt="Fourth slide">
+                            </div>
+                        </div>
+                    </div>
+                    <p class="card-text">I've been to Japan, so worldly.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="columntitle mb-4">
+                Life
+            </div>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">Super deep thoughts on life</h5>
+                    <p class="card-text">This is a very personal website on which I show future employers how wise I am.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
